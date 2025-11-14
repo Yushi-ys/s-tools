0abadd2315d7bbc8473caa427901ca143dcf215c
shitao <18351845337@163.com>
2025-11-14 14:01:42 +0800

    fix: 缓存uuid页面

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,39 +1,56 @@
 import { create } from "zustand";
-import type { StoreState } from "./type";
-import { THEMESTYLE } from "../types/constants";
+import type {
+  IDiffData,
+  ITranslationData,
+  IUuidData,
+  StoreState,
+} from "./type";
+import { THEMESTYLE } from "@/types/constants";
 import type { IClipboardItem } from "@/hooks/useAdvancedClipboard";
 
 const useStore = create<StoreState>((set) => ({
+  // 关闭项目的时候的loading，存数据到本地的sql中使用
   closeWindowLoading: false,
   setCloseWindowLoading: (closeWindowLoading: boolean) =>
     set(() => ({ closeWindowLoading })),
+
   // 主题色
   theme: THEMESTYLE.LIGHT,
   setTheme: (theme: keyof typeof THEMESTYLE) => set(() => ({ theme })),
+
+  // 剪切板相关
   clipBoradData: [],
   setClipBoradData: (data: IClipboardItem[]) =>
     set(() => ({ clipBoradData: data })),
   clipBoradDataLoading: false,
-  setClipBoradDataLoading: (val: boolean) =>
-    set(() => ({ clipBoradDataLoading: val })),
+  setClipBoradDataLoading: (newVal: boolean) =>
+    set(() => ({ clipBoradDataLoading: newVal })),
+
+  // uuid相关
+  uuidData: {
+    uuids: [],
+    character_length: 10,
+    generation_rules: "only_num",
+    sum: 1,
+  },
+  setUuidData: (newVal: IUuidData) => set(() => ({ uuidData: newVal })),
+
+  // diff相关
   diffData: {
     leftContent: "",
     rightContent: "",
   },
-  setDiffData: (newVal: { leftContent: string; rightContent: string }) =>
-    set(() => ({ diffData: newVal })),
+  setDiffData: (newVal: IDiffData) => set(() => ({ diffData: newVal })),
+
+  // 翻译相关
   translationData: {
     originalText: "",
     translatedText: "",
     originalLanguage: "zh",
     translatedlLanguage: "en",
   },
-  setTranslationData: (newVal: {
-    originalText: string;
-    translatedText: string;
-    originalLanguage: string;
-    translatedlLanguage: string;
-  }) => set(() => ({ translationData: newVal })),
+  setTranslationData: (newVal: ITranslationData) =>
+    set(() => ({ translationData: newVal })),
 }));
 
 export default useStore;
